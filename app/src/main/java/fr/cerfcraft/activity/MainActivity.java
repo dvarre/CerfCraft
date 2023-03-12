1e1177edecd7167362cf4efbe12d2f8deb01167b
Dit mitri <varre.dimitri@gmail.com>
2023-03-12 19:57:56 -0400

    Ajout squellete de toutes les activites en partant du menu principal + link des boutons du menu principal vers les activites

--- app/src/main/java/fr/cerfcraft/activity/MainActivity.java
+++ app/src/main/java/fr/cerfcraft/activity/MainActivity.java
@@ -2,8 +2,19 @@ package fr.cerfcraft.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import java.util.ArrayList;
+
+import fr.cerfcraft.BiomesActivity;
+import fr.cerfcraft.CraftActivity;
+import fr.cerfcraft.CuissonActivity;
+import fr.cerfcraft.ItemsActivity;
+import fr.cerfcraft.MissionsActivity;
+import fr.cerfcraft.MobsActivity;
+import fr.cerfcraft.NotesActivity;
 import fr.cerfcraft.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +24,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final int listButtonId[] = {
+                R.id.buttonCraft,
+                R.id.buttonBiomes,
+                R.id.buttonItems,
+                R.id.buttonMobs,
+                R.id.buttonCuisson,
+                R.id.buttonMissions,
+                R.id.buttonNotes
+        };
+        Class listActivityClass[] = {
+                CraftActivity.class,
+                BiomesActivity.class,
+                ItemsActivity.class,
+                MobsActivity.class,
+                CuissonActivity.class,
+                MissionsActivity.class,
+                NotesActivity.class
+        };
 
+        for (int i=0; i<listButtonId.length; i++){
+            Button activityToAcess = findViewById(listButtonId[i]);
+            switch(i){
+                case 1:
+                    activityToAcess.setOnClickListener(v -> {
+                        Intent intent = new Intent(this, BiomesActivity.class );
+                        startActivity(intent);
+                    });
+                    break;
+                case 2:
+                    activityToAcess.setOnClickListener(v -> {
+                        Intent intent = new Intent(this, ItemsActivity.class );
+                        startActivity(intent);
+                    });
+                    break;
+                case 3:
+                    activityToAcess.setOnClickListener(v -> {
+                        Intent intent = new Intent(this, MobsActivity.class );
+                        startActivity(intent);
+                    });
+                    break;
+                case 4:
+                    activityToAcess.setOnClickListener(v -> {
+                        Intent intent = new Intent(this, CuissonActivity.class);
+                        startActivity(intent);
+                    });
+                    break;
+                case 5:
+                    activityToAcess.setOnClickListener(v -> {
+                        Intent intent = new Intent(this, MissionsActivity.class);
+                        startActivity(intent);
+                    });
+                    break;
+                case 6:
+                    activityToAcess.setOnClickListener(v -> {
+                        Intent intent = new Intent(this, NotesActivity.class);
+                        startActivity(intent);
+                    });
+                    break;
+                default:
+                    activityToAcess.setOnClickListener(v -> {
+                        Intent intent = new Intent(this, CraftActivity.class);
+                        startActivity(intent);
+                    });
+                    break;
+            }
+        }
     }
 }
