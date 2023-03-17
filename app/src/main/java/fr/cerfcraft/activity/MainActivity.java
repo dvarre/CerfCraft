5c31a0b082065db5f901bd84c62db9ad14377e31
Megakitraz <megakitraz3000@gmail.com>
2023-03-16 21:58:04 -0400

    Ajout de l'intégration de la BDD

--- app/src/main/java/fr/cerfcraft/activity/MainActivity.java
+++ app/src/main/java/fr/cerfcraft/activity/MainActivity.java
@@ -1,12 +1,21 @@
 package fr.cerfcraft.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import fr.cerfcraft.BiomesActivity;
 import fr.cerfcraft.CraftActivity;
@@ -18,6 +27,8 @@ import fr.cerfcraft.NotesActivity;
 import fr.cerfcraft.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,5 +101,29 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
         }
+
+
+        firestore = FirebaseFirestore.getInstance();
+
+        Map<String, Object> users = new HashMap<>();
+        users.put("firstName","EASY");
+        users.put("lastName","TUTO");
+        users.put("description","Subscribe");
+
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
+
+
 }
