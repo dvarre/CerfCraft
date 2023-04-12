b14c1181ef51b689c4e38cb91517b0d9675b0619
Dit mitri <varre.dimitri@gmail.com>
2023-04-12 01:42:27 -0400

    Ajout deep link sur les biomes

--- app/src/main/java/fr/cerfcraft/BiomeInfo.java
+++ app/src/main/java/fr/cerfcraft/BiomeInfo.java
@@ -26,8 +26,12 @@ import java.util.List;
 
 import fr.cerfcraft.adapter.LinkBiomeAdapter;
 import fr.cerfcraft.model.Biome;
+import fr.cerfcraft.model.Craft;
+import fr.cerfcraft.model.Dimension;
 import fr.cerfcraft.model.Item;
+import fr.cerfcraft.model.Mission;
 import fr.cerfcraft.model.Mob;
+import fr.cerfcraft.model.Structure;
 
 public class BiomeInfo extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -38,11 +42,11 @@ public class BiomeInfo extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        System.out.println(" dans le refresh = " + FixBugRecyclerView.getAucuneIdeeList().size());
+
         RecyclerView recyclerView = findViewById(R.id.links);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        linkBiomeAdapter = new LinkBiomeAdapter(getApplicationContext(), linkObjectList); //TODO probleme ici de liste vide qui est passée. Avant la liste brute j'avais mis ça linkObjectList et ça FixBugRecyclerView.getAucuneIdeeList()
+        linkBiomeAdapter = new LinkBiomeAdapter(getApplicationContext(), linkObjectList);
         recyclerView.setAdapter(linkBiomeAdapter);
     }
 
@@ -79,24 +83,20 @@ public class BiomeInfo extends AppCompatActivity {
 
                     @SuppressLint("UseCompatLoadingForDrawables") Drawable res = getResources().getDrawable(imageResource);
                     imageView.setImageDrawable(res);
-                    List<String> referencelist = biome.getLinks();
-                    System.out.println("referencelist " + referencelist);
-                    String primaryId;
-
-                    for (int i=0; i<referencelist.size(); i++){
-                        db.collection("items").document(referencelist.get(i)).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                           @Override
-                           public void onSuccess(DocumentSnapshot documentSnapshot) {
-                               System.out.println("est-ce qu'il existe " + documentSnapshot.exists());
-
-                               Item obj = documentSnapshot.toObject(Item.class);
-
-                               linkObjectList.add(obj);
-
-                               linkBiomeAdapter.notifyDataSetChanged();
-                           }
-                        });
-                    }
+                    List<String> linksBiomes = biome.getLinksBiomes();
+                    List<String> linksCrafts = biome.getLinksCrafts();
+                    List<String> linksDimensions = biome.getLinksDimensions();
+                    List<String> linksItems = biome.getLinksItems();
+                    List<String> linksMissions = biome.getLinksMissions();
+                    List<String> linksMobs = biome.getLinksMobs();
+                    List<String> linksStructures = biome.getLinksStructures();
+                    addObjectsToListToDisplay(linksBiomes, "biomes", Biome.class);
+                    addObjectsToListToDisplay(linksCrafts, "crafts", Craft.class);
+                    addObjectsToListToDisplay(linksDimensions, "dimensions", Dimension.class);
+                    addObjectsToListToDisplay(linksItems, "items", Item.class);
+                    addObjectsToListToDisplay(linksMissions, "missions", Mission.class);
+                    addObjectsToListToDisplay(linksMobs, "mobs", Mob.class);
+                    addObjectsToListToDisplay(linksStructures, "structures", Structure.class);
                 }
                 else{
                     txtView.setText("");
@@ -107,104 +107,22 @@ public class BiomeInfo extends AppCompatActivity {
 
     }
 
-
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_biome_info);
-//
-//        RecyclerView recyclerView = findViewById(R.id.links);
-//        recyclerView.setHasFixedSize(true);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//
-//        // Chargement de l'élement séléctionné depuis la vue précédente
-//        Intent intent = getIntent();
-//        if (intent != null){
-//            String id ="";
-//            // On regarde si on a bien reçu l'élement de la vue précédente
-//            if(intent.hasExtra("idToDisplay")){
-//                id = intent.getStringExtra("idToDisplay");
-//            }
-//
-//            //On get le document dans la BDD
-//            ref.document(id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//                @Override
-//                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                    if (task.isSuccessful()) {
-//                        db.collection("items").document("oaCUXjVkeX3AtJ8BnIiB").get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//                            @Override
-//                            public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                                Object obj = documentSnapshot.toObject(Object.class);
-//                                System.out.println("liste avant ajout : " + linkObjectList); // vide
-//                                linkObjectList.add(obj);
-//                                System.out.println("liste apres ajout : " + linkObjectList); // Contient l'objet voulu
-//                            }
-//                        });
-//                        // linkObjectList devient vide
-//                    }
-//                    else{
-//                    // Pas important
-//                    }
-//                }
-//            });
-//            linkBiomeAdapter = new LinkBiomeAdapter(getApplicationContext(), linkObjectList);
-//            recyclerView.setAdapter(linkBiomeAdapter);
-//        }
-//        System.out.println("nb d'objets passés : " + linkObjectList.size()); // Toujours égale à 0
-//    }
-
-
-
-    private String cleanPrimaryBDDKey(DocumentReference dref)
-    {
-        String keyClear = dref.toString();
-        int longRef = keyClear.length();
-        for(int i=0;i<longRef;i++){
-            if(keyClear.charAt(i) == '/' && i!=0){
-                keyClear = keyClear.substring(i++,longRef);
-                break;
+    private void addObjectsToListToDisplay(List<String> referencelist, String collectionBD, Class testObj){
+        for (int i=0; i<referencelist.size(); i++){
+            if(referencelist.get(i) != null && referencelist.get(i)!="")
+            {
+                System.out.println("collectionBD = " + collectionBD);
+                System.out.println("referencelist.get(i) = " + referencelist.get(i));
+                db.collection(collectionBD).document(referencelist.get(i)).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Object obj = documentSnapshot.toObject(testObj);
+                        linkObjectList.add(obj);
+                        linkBiomeAdapter.notifyDataSetChanged();
+                    }
+                });
             }
         }
-        System.out.println("keyclear : " + keyClear);
-        return keyClear;
-    }
-
-
-
-    class QueryThread extends Thread {
-        private volatile Object linkObjectList;
-        String id;
-
-
-        QueryThread(String id) {
-            this.id = id;
-        }
-
-        @Override
-        public void run() {
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            System.out.println("nb de fois passe");
-            //TODO régler le probleme de la clé primaire brute
-            db.collection("items").document(id).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                @Override
-                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                    System.out.println("est-ce qu'il existe " + documentSnapshot.exists());
-                    Object obj = documentSnapshot.toObject(Object.class);
-                    linkObjectList = obj;
-                    System.out.println("obj a afficher = " + obj);
-                    System.out.println("liste avant ajout : " + linkObjectList);
-                    System.out.println("liste apres ajout : " + linkObjectList);
-
-                }
-            });
-
-        }
-
-        public Object getLinkObjectList() {
-            return linkObjectList;
-        }
-
     }
 }
 
--- app/src/main/java/fr/cerfcraft/model/Craft.java
+++ app/src/main/java/fr/cerfcraft/model/Craft.java
@@ -13,6 +13,13 @@ public class Craft {
     String name;
     String image;
     String description;
+    List<String> linksBiomes = new ArrayList<>();
+    List<String> linksCrafts = new ArrayList<>();
+    List<String> linksDimensions = new ArrayList<>();
+    List<String> linksItems = new ArrayList<>();
+    List<String> linksMissions = new ArrayList<>();
+    List<String> linksMobs = new ArrayList<>();
+    List<String> linksStructures = new ArrayList<>();
 
     public String getDescription() {
         return description;
@@ -62,7 +69,63 @@ public class Craft {
         return result;
     }
 
-//numSlot between 0 and 8
+    public List<String> getLinksBiomes() {
+        return linksBiomes;
+    }
+
+    public void setLinksBiomes(List<String> linksBiomes) {
+        this.linksBiomes = linksBiomes;
+    }
+
+    public List<String> getLinksCrafts() {
+        return linksCrafts;
+    }
+
+    public void setLinksCrafts(List<String> linksCrafts) {
+        this.linksCrafts = linksCrafts;
+    }
+
+    public List<String> getLinksDimensions() {
+        return linksDimensions;
+    }
+
+    public void setLinksDimensions(List<String> linksDimensions) {
+        this.linksDimensions = linksDimensions;
+    }
+
+    public List<String> getLinksItems() {
+        return linksItems;
+    }
+
+    public void setLinksItems(List<String> linksItems) {
+        this.linksItems = linksItems;
+    }
+
+    public List<String> getLinksMissions() {
+        return linksMissions;
+    }
+
+    public void setLinksMissions(List<String> linksMissions) {
+        this.linksMissions = linksMissions;
+    }
+
+    public List<String> getLinksMobs() {
+        return linksMobs;
+    }
+
+    public void setLinksMobs(List<String> linksMobs) {
+        this.linksMobs = linksMobs;
+    }
+
+    public List<String> getLinksStructures() {
+        return linksStructures;
+    }
+
+    public void setLinksStructures(List<String> linksStructures) {
+        this.linksStructures = linksStructures;
+    }
+
+    //numSlot between 0 and 8
 //    public Item getSlot(int numSlot){
 //        if((numSlot>=0) && (numSlot<9)){
 //            return slots[numSlot];
